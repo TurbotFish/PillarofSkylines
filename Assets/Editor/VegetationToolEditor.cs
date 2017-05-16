2ddc43009b364df25ba046d226e438bc03fc283d
Antoine Villecroze <antoine.villecroze@gmail.com>
2017-05-16 23:58:53 +0200

    VegetationTool : v0.2

--- Assets/Editor/VegetationToolEditor.cs
+++ Assets/Editor/VegetationToolEditor.cs
@@ -6,16 +6,46 @@ using UnityEditor;
 [CustomEditor(typeof(VegetationTool))]
 public class VegetationToolEditor : Editor {
 
+	GUIStyle redButton;
+	GUIStyle greenButton;
+	Texture2D greenTexture;
+	Texture2D redTexture;
+
+	public void OnEnable()
+	{
+		redTexture = new Texture2D(1, 1, TextureFormat.RGBA32, false);
+		redTexture.SetPixel(0, 0, Color.red);
+		redTexture.Apply();
+
+		greenTexture = new Texture2D(1, 1, TextureFormat.RGBA32, false);
+		greenTexture.SetPixel(0, 0, Color.green);
+		greenTexture.Apply();
+	}
+
 	public override void OnInspectorGUI()
 	{
+		greenButton = new GUIStyle(GUI.skin.button);
+		greenButton.normal.background = greenTexture;
+		redButton = new GUIStyle(GUI.skin.button);
+		redButton.normal.background = redTexture;
+
+
 		DrawDefaultInspector();
+		EditorGUILayout.Space();
+		EditorGUILayout.Space();
+		EditorGUILayout.Space();
+
 		VegetationTool tool = (VegetationTool)target;
-		if (GUILayout.Button("Instantiate Vegetation"))
+		if (GUILayout.Button("Instantiate Vegetation", greenButton))
 		{
 			tool.InstantiateVegetation();
 		}
 
-		if (GUILayout.Button("Delete All Groups"))
+		EditorGUILayout.Space();
+		EditorGUILayout.Space();
+		EditorGUILayout.Space();
+
+		if (GUILayout.Button("Delete All Groups", redButton))
 		{
 			List<GameObject> children = new List<GameObject>();
 			foreach(Transform child in tool.transform) children.Add(child.gameObject);
--- Assets/Scripts/VegetationTool.cs
+++ Assets/Scripts/VegetationTool.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(SphereCollider))]
 public class VegetationTool : MonoBehaviour {
 
+	[Header("Global Param")]
 	public int density = 20;
-
-	public float posChange = 0.3f;
-
+	public float posRandom = 0.3f;
+	public float heightRandom = 0.1f;
+	[Space()]
+	[Header("Normal Target")]
+	public bool useNormalTarget = false;
+	public Vector3 normalTarget = new Vector3(0,1,0);
+	public float AngleMargin = 10;
 
 	[System.Serializable]
 	public class VegeElement
@@ -16,9 +21,10 @@ public class VegetationTool : MonoBehaviour {
 		public GameObject vegePrefab;
 		public MinMax scaleChange = new MinMax(0.5f,1.5f);
 		[Range(0,100)]
-		public float proportion;
+		public int proportion;
 	}
-
+	[Space()]
+	[Header("Spawnable Elements")]
 	public List<VegeElement> elements = new List<VegeElement>();
 
 	public void InstantiateVegetation ()
@@ -27,6 +33,7 @@ public class VegetationTool : MonoBehaviour {
 		Vector3 center = col.bounds.center;
 		float radius = col.radius*transform.localScale.x;
 		float rotator = 2*Mathf.PI / density;
+		Vector3 newCenter = center+Vector3.up*radius*0.5f;
 
 		RaycastHit hit;
 		GameObject vegeGroup = new GameObject("vegetationGroup");
@@ -34,6 +41,15 @@ public class VegetationTool : MonoBehaviour {
 
 		float totalProportion = 0;
 		foreach (VegeElement elem in elements) totalProportion += elem.proportion;
+		List<int> proportions = new List<int>();
+		for (int k = 0; k<elements.Count;k++)
+		{
+			proportions.Add(elements[k].proportion);
+			if(k>0)
+			{
+				proportions[k] += proportions[k-1];
+			}
+		}
 
 		for (int i = 1; i < density ; i++)
 		{
@@ -44,45 +60,38 @@ public class VegetationTool : MonoBehaviour {
 				float z = center.z + radius * Mathf.Cos(rotator*j);
 				Vector3 pointOnSphere = new Vector3(x,y,z);
 
-				Vector3 dir = pointOnSphere - center;
+				Vector3 dir = pointOnSphere - newCenter;
+				float dist = Mathf.Abs((pointOnSphere-newCenter).magnitude);
+				//Debug.DrawRay(newCenter,dir,Color.red,3);
 
-				if (Physics.Raycast(center, dir, out hit, radius))
+				if (Physics.Raycast(newCenter, dir, out hit, dist))
 				{
 					Vector3 normal = hit.normal;
-
-					//choose which vege element should be used :
-					float rand = Random.Range(0,totalProportion);
-					//Debug.Log("rand : " + rand);
-					int chosenElem = 0;
-					for (int k = 0; k<elements.Count;k++)
+					if (!useNormalTarget || (useNormalTarget && Vector3.Angle (normal,normalTarget) < AngleMargin+1))
 					{
-						float intermediaryTotal = 0;
-						for (int l = 0; l <= k; l++)
+						//choose which vege element should be used :
+						float rand = Random.Range(0,totalProportion);
+						int chosenElem = 0;
+						for (int k = 0; k<elements.Count;k++)
 						{
-							intermediaryTotal += elements[l].proportion;
-							//Debug.Log (intermediaryTotal);
+							if (rand < proportions[k])
+							{
+								chosenElem = k;
+								break;
+							}
+							else
+							{
+								continue;
+							}
 						}
-
-						if (rand < intermediaryTotal)
-						{
-							chosenElem = k;
-							break;
-						}
-						else
-						{
-							continue;
-						}
+						GameObject instance = Instantiate(elements[chosenElem].vegePrefab, hit.point, Quaternion.FromToRotation(Vector3.up,normal)) as GameObject;
+						instance.transform.localScale *= Random.Range(elements[chosenElem].scaleChange.min, elements[chosenElem].scaleChange.max);
+						instance.transform.position += instance.transform.forward *Random.Range(-posRandom,posRandom) + instance.transform.right *Random.Range(-posRandom,posRandom) + transform.up*Random.Range(0,heightRandom);
+						instance.transform.RotateAround(instance.transform.position,normal,Random.Range(0,360));
+						instance.transform.parent = vegeGroup.transform;
 					}
-					GameObject instance = Instantiate(elements[chosenElem].vegePrefab, hit.point, Quaternion.FromToRotation(Vector3.up,normal)) as GameObject;
-					instance.transform.localScale *= Random.Range(elements[chosenElem].scaleChange.min, elements[chosenElem].scaleChange.max);
-					instance.transform.position += instance.transform.forward *Random.Range(-posChange,posChange) + instance.transform.right *Random.Range(-posChange,posChange);
-					instance.transform.RotateAround(instance.transform.position,normal,Random.Range(0,360));
-					instance.transform.parent = vegeGroup.transform;
-					//Debug.Log("pass");
 				}
 			}
-		
 		}
-
 	}
 }
