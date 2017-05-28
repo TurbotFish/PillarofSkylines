7b5a7e22d0a0f7428bd194d4f0d72a25147102e8
Antoine Villecroze <antoine.villecroze@gmail.com>
2017-05-28 21:18:14 +0200

    Swithc between grrTests and Presentation + shortcuts

---
+++ Assets/Presentations/Scripts/PresentationShortcutsV2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PresentationShortcutsV2 : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			string _scene = SceneManager.GetActiveScene().name;
+			SceneManager.LoadSceneAsync(_scene);
+		}
+	}
+}
---
+++ Assets/Presentations/Scripts/ProtoSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProtoSwitch : MonoBehaviour {
+
+
+	public bool inPrez = true;
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			if(inPrez)
+			{
+				SceneManager.LoadSceneAsync("grrTests");
+			}	
+			if(!inPrez)
+			{
+				SceneManager.LoadSceneAsync("PresentationJury");
+			}	
+		}
+	}
+}
