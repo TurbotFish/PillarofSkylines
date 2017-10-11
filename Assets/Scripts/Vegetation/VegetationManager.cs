1dd95580328be0e8b3ff99967eab39bcf43dd5c2
Unknown <antoine.villecroze@gmail.com>
2017-10-11 17:38:32 +0200

    Starting experimentation with plants physics

---
+++ Assets/Scripts/Vegetation/VegetationManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VegetationManager : MonoBehaviour {
+
+	public Vector3 wind;
+	public float waveMultiplier;
+	float _waveFactor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//_waveFactor = Mathf.Sin(Time.time * waveMultiplier)
+	}
+}
