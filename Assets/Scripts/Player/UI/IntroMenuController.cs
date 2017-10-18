c56e3de0748296730f6c12bb095e8d80ef6f4bf7
Namerian <email@email.shit>
2017-10-18 17:20:56 +0200

    work on ui

---
+++ Assets/Scripts/Player/UI/IntroMenuController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Player.UI
+{
+    public class IntroMenuController : MonoBehaviour, IUiState
+    {
+        public bool IsActive { get; private set; }
+
+        //###########################################################
+
+        #region monobehaviour methods
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #endregion monobehaviour methods
+
+        //###########################################################
+
+        void IUiState.Activate()
+        {
+            if (this.IsActive)
+            {
+                return;
+            }
+
+            this.IsActive = true;
+            this.gameObject.SetActive(true);
+
+            Utilities.EventManager.SendOnMenuOpenedEvent(this, new Utilities.EventManager.OnMenuOpenedEventArgs(eUiState.Intro));
+            Time.timeScale = 0.1f;
+        }
+
+        void IUiState.Deactivate()
+        {
+            bool wasActive = this.IsActive;
+
+            this.IsActive = false;
+            this.gameObject.SetActive(false);
+
+            if (wasActive)
+            {
+                Utilities.EventManager.SendOnMenuClosedEvent(this, new Utilities.EventManager.OnMenuClosedEventArgs(eUiState.Intro));
+                Time.timeScale = 1;
+            }
+        }
+
+        //###########################################################
+    }
+} //end of namespace
