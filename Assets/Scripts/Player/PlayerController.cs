071f430fcee010406b30bfe1a52fc9f20ece2dc5
Namerian <email@email.shit>
2017-10-27 10:50:12 +0200

    added GameControllerLite and PlayerController

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Player
+{
+    /// <summary>
+    /// This class handles the Initialization of the Player Prefab.
+    /// This class MUST be at the root of the Player Prefab!
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        global::Player player;
+        public global::Player Player { get { return this.player; } }
+
+        [SerializeField]
+        FavourController favourController;
+        public FavourController FavourController { get { return this.favourController; } }      
+
+        /// <summary>
+        /// Initializes the Player Prefab with references to outside scripts.
+        /// </summary>
+        /// <param name="gameController"></param>
+        public void InitializePlayerController(GameControllerBase gameController)
+        {
+            //this.player.InitializePlayer(gameController.PlayerModel);
+            this.favourController.InitializeFavourController(gameController.PlayerModel);
+        }
+    }
+}
