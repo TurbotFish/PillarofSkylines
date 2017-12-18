c8290f7513de90b7c1f66a888c4eb3f803e5e743
grreuze <grreuze@gmail.com>
2017-12-18 11:48:12 +0100

    jamais été aussi ready to merge

---
+++ Assets/Scripts/MoveToPosition.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToPosition : MonoBehaviour {
+
+    [SerializeField] Vector3 targetPos;
+    [SerializeField] AnimationCurve curve;
+    [SerializeField] float duration = 30;
+
+    Transform my;
+    Vector3 startPos;
+
+    void Start () {
+        my = transform;
+        startPos = my.localPosition;
+        StartCoroutine(Move());
+	}
+
+    IEnumerator Move() {
+
+        for (float elapsed = 0; elapsed < duration; elapsed+=Time.deltaTime) {
+            float t = curve.Evaluate(elapsed / duration);
+            my.localPosition = Vector3.Lerp(startPos, targetPos, t);
+            yield return null;
+        }
+
+    }
+}
