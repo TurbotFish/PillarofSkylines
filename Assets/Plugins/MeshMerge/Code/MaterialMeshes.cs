1e75b594dadb914b5477a37930baac72ae61cbf7
ETU\a.villecroze <antoine.villecroze@gmail.com>
2017-12-18 08:48:01 +0100

    save

---
+++ Assets/Plugins/MeshMerge/Code/MaterialMeshes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.ootii.MeshMerge
+{
+    /// <summary>
+    /// Container for holding mesh filters that are tied to a 
+    /// specific material.
+    /// </summary>
+    public class MaterialMeshes
+    {
+        public Material Material;
+
+        public List<MeshFilter> MeshFilters = new List<MeshFilter>();
+
+        public List<Mesh> Meshes = new List<Mesh>();
+
+        public List<Matrix4x4> Transform = new List<Matrix4x4>();
+    }
+}
---
+++ Assets/Plugins/MeshMerge/Code/MergedMesh.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.ootii.MeshMerge
+{
+    /// <summary>
+    /// Container to store the mesh filter IDs that are used
+    /// to create this mesh.
+    /// </summary>
+    public class MergedMesh : MonoBehaviour
+    {
+        /// <summary>
+        /// IDs of the objects that were merged into this object
+        /// </summary>
+        public int[] MeshFilterIDs = null;
+    }
+}
