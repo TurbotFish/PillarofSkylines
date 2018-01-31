b177a19eebd9cbb177dfd7c5b7ef046676ae764a
Alo <a.mispelon@rubika-edu.com>
2018-01-31 17:12:47 +0100

    GPU instancing dans hubert

--- Assets/Graph/Shaders/UberShader/Editor/CustomPBR_GUI.cs
+++ Assets/Graph/Shaders/UberShader/Editor/CustomPBR_GUI.cs
@@ -39,6 +39,8 @@ public class CustomPBR_GUI : ShaderGUI {
 		DoRenderQueue ();
 
 		//Debug.Log ("HELLOOOO");
+
+		DoAdvanced ();
 	}
 
 	void DoMain(){
@@ -685,6 +687,11 @@ public class CustomPBR_GUI : ShaderGUI {
 		if (avatarShader) {
 			target.renderQueue = (int)RenderQueue.Geometry+501;
 		}
+	}
+
+	void DoAdvanced(){
+		GUILayout.Label ("Advanced options", EditorStyles.boldLabel);
+		editor.EnableInstancingField();
 	}
 
 	#region debug
