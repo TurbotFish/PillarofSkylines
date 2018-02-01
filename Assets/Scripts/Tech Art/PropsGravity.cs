eab448f7b067e66d4dbda7f5517dbaf7be6aba54
ETU\a.villecroze <antoine.villecroze@gmail.com>
2018-02-01 15:16:12 +0100

    Habillage ville wip

---
+++ Assets/Scripts/Tech Art/PropsGravity.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropsGravity : MonoBehaviour {
+
+	public enum gravityFields {EastSurface,WestSurface, None}
+	public gravityFields gravityField;
+	public float gravityForce = 9.81f; 
+	Rigidbody rb;
+
+	public bool useGravity;
+
+	//gravity vectors
+	Vector3 gravityWest, gravityEast;
+
+	void Awake ()
+	{
+		useGravity = false;
+		gravityWest = new Vector3 (gravityForce,0,0);
+		gravityEast = new Vector3 (-gravityForce,0,0);
+		rb = this.GetComponent<Rigidbody> ();
+	}
+
+	void Start ()
+	{
+		StartCoroutine (StartGravity ());
+	}
+
+	void FixedUpdate () {
+
+		if (useGravity) {
+			if (gravityField == gravityFields.EastSurface) {
+				rb.AddForce (gravityEast);
+			} else if (gravityField == gravityFields.WestSurface) {
+				rb.AddForce (gravityWest);
+			}
+		}
+	}
+
+	IEnumerator StartGravity()
+	{
+		yield return new WaitForSecondsRealtime (5);
+		useGravity = true;
+	}
+
+}
