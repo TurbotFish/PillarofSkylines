d6ba2b7527a2dad784f41ce09b4477d684b733ad
Unknown <flavien.birette@orange.fr>
2018-02-10 11:53:14 +0100

    Snap vers gravités normales, reset gravité double clic stick droit

--- Assets/Scripts/Player/CharacterController/States/GraviswapState.cs
+++ Assets/Scripts/Player/CharacterController/States/GraviswapState.cs
@@ -16,6 +16,7 @@ namespace Game.Player.CharacterController.States
         CharData.StandData standData;
 
         Vector3 initialVelocity;
+        float timerGravDefault;
 
         //#############################################################################
 
@@ -30,6 +31,7 @@ namespace Game.Player.CharacterController.States
 
         public void Enter()
         {
+            timerGravDefault = .5f;
             Debug.Log("Enter State: Graviswap");
             initialVelocity = charController.MovementInfo.velocity;
             charController.SetVelocity(Vector3.zero, true);
@@ -37,6 +39,52 @@ namespace Game.Player.CharacterController.States
 
         public void Exit()
         {
+
+            Debug.Log("timer : " + timerGravDefault);
+            if (timerGravDefault > 0f)
+            {
+                Debug.Log("hello back to default");
+                charController.ChangeGravityDirection(Vector3.down);
+            } else
+            {
+                float xSnap = 0f, ySnap = 0f, zSnap = 0f;
+                Debug.Log("eulers : " + charController.MyTransform.eulerAngles);
+                if (charController.MyTransform.eulerAngles.x % 90 < 10)
+                {
+                    xSnap = -charController.MyTransform.eulerAngles.x % 90;
+                    Debug.Log("x snap");
+                }
+                if (charController.MyTransform.eulerAngles.x % 90 > 80)
+                {
+                    xSnap = 90 - charController.MyTransform.eulerAngles.x % 90;
+                    Debug.Log("x snap");
+                }
+                if (charController.MyTransform.eulerAngles.y % 90 < 10)
+                {
+                    ySnap = -charController.MyTransform.eulerAngles.y % 90;
+                    Debug.Log("y snap");
+                }
+                if (charController.MyTransform.eulerAngles.y % 90 > 80)
+                {
+                    ySnap = 90 - charController.MyTransform.eulerAngles.y % 90;
+                    Debug.Log("y snap");
+                }
+                if (charController.MyTransform.eulerAngles.z % 90 < 10)
+                {
+                    zSnap = -charController.MyTransform.eulerAngles.z % 90;
+                    Debug.Log("z snap");
+                }
+                if (charController.MyTransform.eulerAngles.z % 90 > 80)
+                {
+                    zSnap = 90 - charController.MyTransform.eulerAngles.z % 90;
+                    Debug.Log("z snap");
+                }
+                charController.MyTransform.eulerAngles = new Vector3(charController.MyTransform.eulerAngles.x + xSnap
+                                                                    , charController.MyTransform.eulerAngles.y + ySnap
+                                                                    , charController.MyTransform.eulerAngles.z + zSnap);
+            }
+            
+            
             Debug.Log("Exit State: Graviswap");
         }
 
@@ -59,6 +107,8 @@ namespace Game.Player.CharacterController.States
 
         public StateReturnContainer Update(float dt)
         {
+
+            timerGravDefault -= Time.deltaTime;
 
             PlayerInputInfo inputInfo = charController.InputInfo;
 
