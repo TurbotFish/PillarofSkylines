980d6d48478eb5e19c41b3ae2120f20d5d80e344
Alo <a.mispelon@rubika-edu.com>
2018-02-13 11:13:14 +0100

    je vais faire un truc de ouf, alors je sauvegarde ici

--- Assets/Scenes/Aloïs/GPUInstancing/BetterWayOfDoingThings/GPUIDisplayManager.cs
+++ Assets/Scenes/Aloïs/GPUInstancing/BetterWayOfDoingThings/GPUIDisplayManager.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 
 public class GPUIDisplayManager : MonoBehaviour {
 
+	//Ce script a été optimisé avec l'esprit de Bruno Mabille
+
+	public enum GPUIMethod {Method1, Method2, Method3};
+	public GPUIMethod GPUI;
+
+
 	Dictionary<int, List<Matrix4x4>> transformsID = new Dictionary<int, List<Matrix4x4>> ();
 	List<Matrix4x4> matrices = new List<Matrix4x4> ();
 	List<int> indices = new List<int> ();
@@ -10,18 +16,25 @@ public class GPUIDisplayManager : MonoBehaviour {
 	public static GPUIDisplayManager displayManager;
 
 	int numberOfCalls;
-	//int[,] boundaries = {{0,1023},{1024,2047}, };
 	int boundaryLow;
 	int boundaryHigh;
+	int instancesPerList;
+
+	List<Matrix4x4>[] matrices1023 = new List<Matrix4x4>[80];
+
+	bool updatedThisFrame;
 
 	//temp, should be on seeder
 	public Mesh meshToDraw;
 	public Material materialToDraw;
 
-	int batchSize = 1023;
 
-	void Start(){
+	void Awake(){
 		displayManager = this;
+
+		for (int i = 0; i < matrices1023.Length; i++) {
+			matrices1023 [i] = new List<Matrix4x4> ();
+		}
 	}
 
 	public void AddStuffToDraw(List<Matrix4x4> _mat, int _id){
@@ -36,10 +49,10 @@ public class GPUIDisplayManager : MonoBehaviour {
 		RearrangeListOfObjectsToDraw ();
 	}
 
-	void Update(){
+	void LateUpdate(){
+		
 		GPUIDraw();
-
-		//Debug.Log (matrices.Count);
+		updatedThisFrame = false;
 	}
 
 	void GPUIDraw(){
@@ -48,31 +61,52 @@ public class GPUIDisplayManager : MonoBehaviour {
 			return;
 
 		for (int i = 0; i < numberOfCalls; i++) {
-			boundaryLow = i * 1023;
-			//boundaryHigh = numberOfCalls * 1024 - 1;
-			boundaryHigh = Mathf.Min (1023, matrices.Count - boundaryLow);
+			#region method1
+			if(GPUI == GPUIMethod.Method1){
+				boundaryLow = i * 1023;
+				boundaryHigh = Mathf.Min (1023, matrices.Count - boundaryLow);
+				Graphics.DrawMeshInstanced (meshToDraw, 0, materialToDraw, matrices.GetRange (boundaryLow, boundaryHigh), null, UnityEngine.Rendering.ShadowCastingMode.Off, false, 0, null);
+			}
+			#endregion
 
-			//Debug.Log ("low:  " + boundaryLow + "   high:  " + boundaryHigh);
-
-			//Graphics.DrawMeshInstanced (meshToDraw, 0, materialToDraw, matrices.GetRange (boundaryLow, boundaryHigh));
-			Graphics.DrawMeshInstanced(meshToDraw,0,materialToDraw,matrices.GetRange (boundaryLow, boundaryHigh),null,UnityEngine.Rendering.ShadowCastingMode.Off, false, 0, null);
+			#region method2
+			if(GPUI == GPUIMethod.Method2){
+				Graphics.DrawMeshInstanced (meshToDraw, 0, materialToDraw, matrices1023[i], null, UnityEngine.Rendering.ShadowCastingMode.Off, false, 0, null);
+			}
+			#endregion
 		}
-		//Graphics.DrawMeshInstanced (meshToDraw, 0, materialToDraw, matrices.GetRange (0, 1023));
-		//Graphics.DrawMeshInstanced(meshToDraw, 0,materialToDraw, matrices.ToArray(), 1023, null, UnityEngine.Rendering.ShadowCastingMode.Off, false);
 	}
 
 	void RearrangeListOfObjectsToDraw(){
 		matrices.Clear ();
-		foreach (List<Matrix4x4> item in transformsID.Values) {
-			matrices.AddRange (item);
+
+//		foreach (List<Matrix4x4> item in transformsID.Values) {
+//			matrices.AddRange (item);
+//		}
+
+		//replace foreach
+		for (int i = 0; i < indices.Count; i++) {
+			matrices.AddRange (transformsID[indices[i]]);
 		}
-			//replace foreach
-		//		for (int i = 0; i < indices.Count; i++) {
-		//			matrices.AddRange (transformsID[indices[i]]);
-		//		}
 
 		numberOfCalls = matrices.Count / 1024 + 1;
 		//Debug.Log ("calls : "+numberOfCalls+"    vertices : "+matrices.Count);
 
+
+		#region method2
+		if(GPUI == GPUIMethod.Method2){
+			//method 2: smoother when seeders don't change too often
+			for (int i = 0; i < numberOfCalls; i++) {
+				boundaryLow = i * 1023;
+				instancesPerList = Mathf.Min (1023, matrices.Count - boundaryLow);
+
+				matrices1023 [i].Clear ();
+				matrices1023 [i].AddRange (matrices.GetRange (boundaryLow, instancesPerList));
+			}
+		}
+		#endregion
+
+
 	}
+
 }
