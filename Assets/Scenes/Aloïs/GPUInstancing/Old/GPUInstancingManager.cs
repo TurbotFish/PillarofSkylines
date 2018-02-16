bbd8248b59cfe319dead686c4959643aa430a86b
Alo <a.mispelon@rubika-edu.com>
2018-02-16 09:34:43 +0100

    un peu de ménage bah oui

---
+++ Assets/Scenes/Aloïs/GPUInstancing/Old/GPUInstancingManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GPUInstancingManager : MonoBehaviour {
+
+	public GPUInstancingBehaviour[] behaviours;
+
+	void Update(){
+		for (int i = 0; i < behaviours.Length; i++) {
+			DrawAllTheMeshes (behaviours [i]);
+		}
+	}
+
+	void DrawAllTheMeshes(GPUInstancingBehaviour _gpui){
+		Graphics.DrawMeshInstanced (_gpui.mesh, _gpui.subMeshIndex, _gpui.material, _gpui.matrices, _gpui.instances, null, _gpui.shadowCasting, _gpui.receiveShadows, _gpui.layer);
+	}
+}
