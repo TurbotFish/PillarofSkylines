cf9758b436e17ed44027992605781c5e68816671
ETU\a.villecroze <antoine.villecroze@gmail.com>
2018-04-11 11:04:57 +0200

    fixes de trucs dans l'open world + progress sur les pillar marks

--- Assets/Scripts/Tech Art/PillarMarkFX.cs
+++ Assets/Scripts/Tech Art/PillarMarkFX.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PillarMarkFX : MonoBehaviour {
 
 	public Animator eyeAnim;
+	public float timeBeforeChange;
+	public Material crystalOff;
+	public List<MeshRenderer> crystalsTransforming = new List<MeshRenderer>();
+	public List<MeshRenderer> crystalsImmediate = new List<MeshRenderer>();
+
+	public List<ParticleSystemRenderer> crystalParticles = new List<ParticleSystemRenderer>();
+
 
 	public void GetMark()
 	{
 		eyeAnim.SetBool ("marked", true);
+		StartCoroutine (EndAnimation ());
 	}
 	
 	void Update () {
@@ -16,4 +24,23 @@ public class PillarMarkFX : MonoBehaviour {
 			GetMark ();
 		}	
 	}
+
+	IEnumerator EndAnimation()
+	{
+		yield return new WaitForSecondsRealtime (timeBeforeChange);
+		foreach (MeshRenderer ms in crystalsImmediate) {
+			ms.material = crystalOff;
+		}
+		for (int i = 0; i < 400; i++) {
+			yield return new WaitForSeconds (0.01f);
+			foreach (MeshRenderer ms in crystalsTransforming) {
+				Material mat = ms.material;
+				mat.SetFloat ("_Transition", mat.GetFloat ("_Transition") - 0.1f);
+			}
+		}
+		foreach (ParticleSystemRenderer psr in crystalParticles) {
+			psr.material = crystalOff;
+		}
+	}
+
 }
