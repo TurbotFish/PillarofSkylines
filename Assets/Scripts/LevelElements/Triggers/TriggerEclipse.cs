d5511f62dfcff4f51b03e2548dda0d081d0ac972
Unknown <flavien.birette@orange.fr>
2018-05-14 17:30:43 +0200

    Pi lard troua

---
+++ Assets/Scripts/LevelElements/Triggers/TriggerEclipse.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game.GameControl;
+
+namespace Game.LevelElements
+{
+    public class TriggerEclipse : Trigger
+    {
+
+        //###########################################################
+
+        #region private methods
+
+        void OnEclipseEventHandler(object sender, Game.Utilities.EventManager.EclipseEventArgs args)
+        {
+            
+            if (args.EclipseOn)
+                SetTriggerState(false, true);
+            else
+                SetTriggerState(true, true);
+
+        }
+
+        #endregion private methods
+
+        //###########################################################
+
+        #region monobehaviour methods
+
+        private void OnDisable()
+        {
+            Game.Utilities.EventManager.EclipseEvent -= OnEclipseEventHandler;
+        }
+        
+        #endregion monobehaviour methods
+
+        //###########################################################
+
+        #region public methods
+
+        public override void Initialize(IGameControllerBase gameController)
+        {
+            base.Initialize(gameController);
+            //Debug.Log ("INITIALISE" + transform.name);
+            Game.Utilities.EventManager.EclipseEvent += OnEclipseEventHandler;
+        }
+
+        #endregion public methods
+
+        //###########################################################
+    }
+} //end of namespace
