081dee0d62da99228d8e263139f5568e76649cc4
Unknown <flavien.birette@orange.fr>
2018-05-29 17:12:59 +0200

    Glide trigger cool et tout

--- Assets/Scripts/Player/CharacterController/States/AirState.cs
+++ Assets/Scripts/Player/CharacterController/States/AirState.cs
@@ -168,7 +168,7 @@ namespace Game.Player.CharacterController.States
                 stateMachine.ChangeState(new JetpackState(charController, stateMachine));
             }
             //glide
-            else if (inputInfo.glideButtonDown && !stateMachine.CheckStateLocked(ePlayerState.glide)) {
+            else if ((inputInfo.glideButtonDown || (firstUpdate && inputInfo.glideButton)) && !stateMachine.CheckStateLocked(ePlayerState.glide)) {
 				stateMachine.ChangeState(new GlideState(charController, stateMachine));
 			}
             //landing on slope
