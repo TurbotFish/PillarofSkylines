3d69d53fa0507c769c7881133391e4011ca4d0e4
grreuze <grreuze@gmail.com>
2018-06-01 16:15:28 +0200

    fixed Pillar Mark Tombs

--- Assets/Scripts/LevelElements/Pickups/PillarKeyPickup.cs
+++ Assets/Scripts/LevelElements/Pickups/PillarKeyPickup.cs
@@ -17,7 +17,7 @@ namespace Game.LevelElements
 
         public override string PickupName { get { return "Pillar Key"; } }
         public override string OnPickedUpMessage { get { return "The Eyes have marked you"; } }
-        public override string OnPickedUpDescription { get { return "Destroy the Pillars to free the world"; } }
+        public override string OnPickedUpDescription { get { return "Break the Pillars to free the world"; } }
 
         //##################################################################
 
