9a68f0e1ee1e3f986b98c539ca38050a5f74bf7a
Alo <a.mispelon@rubika-edu.com>
2018-06-01 14:29:35 +0200

    pilou a retrouvé ses chaussures à talons

--- Assets/Scenes/Aloïs/SoundStuff/PilouSounds.cs
+++ Assets/Scenes/Aloïs/SoundStuff/PilouSounds.cs
@@ -12,7 +12,18 @@ public class PilouSounds : MonoBehaviour {
 	int groundType;
 	int groundFromRaycast;
 
-	public void FootSound(AnimationEvent _event){
+
+	float currentFootLeft;
+	float lastFootLeft;
+	float currentFootRight;
+	float lastFootRight;
+
+	Animator anim;
+
+	void Update(){
+
+		if (!anim)
+			anim = this.GetComponent<Animator> ();
 
 		if (!audioManager)
 			audioManager = FindObjectOfType<AudioManager> ();
@@ -28,6 +39,49 @@ public class PilouSounds : MonoBehaviour {
 			if (!leftFoot)
 				Debug.LogError ("Couldn't find right foot object, it's likely the hierarchy has been changed.");
 		}
+
+		currentFootLeft = anim.GetFloat ("LeftFoot");
+		if (currentFootLeft < 0f && lastFootLeft >= 0f) {
+			if(CheckGround(leftFoot.transform.position + transform.up * .3f))
+				audioManager.PlayRandomFootstep (groundFromRaycast);
+				
+		}
+		lastFootLeft = currentFootLeft;
+
+		currentFootRight = anim.GetFloat ("RightFoot");
+		if (currentFootRight >= 0f && lastFootRight < 0f) {
+			if(CheckGround(rightFoot.transform.position + transform.up * .3f))
+				audioManager.PlayRandomFootstep (groundFromRaycast);
+		}
+		lastFootRight = currentFootRight;
+		
+	}
+
+	bool CheckGround(Vector3 _rayOrigin){
+		groundFromRaycast = 0;
+
+		Debug.DrawRay (_rayOrigin, -transform.up * .8f, Color.cyan);
+
+		bool _hitSomething = Physics.Raycast (_rayOrigin, -transform.up, out rayHit, .8f, groundMask);
+
+		if (_hitSomething) {
+			
+			string _objectTag = rayHit.transform.gameObject.tag;
+
+			if (_objectTag == "Grass") {
+				groundFromRaycast = 2;
+			} else {
+				groundFromRaycast = 1;
+			}
+		}
+
+		return _hitSomething;
+	}
+
+	/*
+	public void FootSound(AnimationEvent _event){
+
+		
 
 		soundDebug04 = transform.position;
 		//print (_event.animatorClipInfo.weight + "    " + Time.frameCount);
@@ -68,13 +122,13 @@ public class PilouSounds : MonoBehaviour {
 		if (Physics.Raycast (rayOrigin, -transform.up, out rayHit, .8f, groundMask)) {
 			string _objectTag = rayHit.transform.gameObject.tag;
 
-			/*
-			if (_objectTag == "Concrete") {
-				groundFromRaycast = 1;
-			} else if (_objectTag == "Grass") {
-				groundFromRaycast = 2;
-			} 
-			*/
+			
+//			if (_objectTag == "Concrete") {
+//				groundFromRaycast = 1;
+//			} else if (_objectTag == "Grass") {
+//				groundFromRaycast = 2;
+//			} 
+
 			soundDebug01 = rayHit.point;
 
 			if (_objectTag == "Grass") {
@@ -103,4 +157,5 @@ public class PilouSounds : MonoBehaviour {
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawCube (soundDebug04, Vector3.one * .2f);
 	}
+*/
 }
