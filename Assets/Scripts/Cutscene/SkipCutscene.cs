dfec125136e6ee8db29884fca3692dfaa3f9ed32
grreuze <grreuze@gmail.com>
2018-06-07 16:07:41 +0200

    Skippable Cutscenes

---
+++ Assets/Scripts/Cutscene/SkipCutscene.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SkipCutscene : MonoBehaviour {
+
+    [SerializeField] GameObject[] toDisable;
+
+    [SerializeField] Transform playerPos;
+
+    [SerializeField] Transform moveCrystal;
+
+    [SerializeField] float crystalYMove = 69.9f;
+
+    [SerializeField] Animator pfAnimator;
+
+    [SerializeField] Game.Cutscene.Cutscene cutscene;
+
+    bool active;
+
+    public void EnableCutsceneSkip() {
+        active = true;
+    }
+
+
+	void Update () {
+		
+        if (active && Input.GetButton("MenuButton")) {
+            foreach (GameObject go in toDisable)
+                go.SetActive(false);
+
+            Transform player = FindObjectOfType<Game.Player.PlayerController>().transform;
+            player.position = playerPos.position;
+            player.rotation = playerPos.rotation;
+
+            if (pfAnimator)
+                pfAnimator.SetBool("Activate", true);
+
+            if (moveCrystal)
+                moveCrystal.position -= Vector3.up * crystalYMove;
+
+            cutscene.OnCutsceneOver();
+
+            Destroy(gameObject);
+        }
+        
+	}
+
+}
