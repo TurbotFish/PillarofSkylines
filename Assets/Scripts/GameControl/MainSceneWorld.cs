551d1654a5f42c0d46f6a9a25c16565f3c0f617c
Namerian <email@email.shit>
2018-06-15 14:24:00 +0200

    MainSceneWorld is now initialized

---
+++ Assets/Scripts/GameControl/MainSceneWorld.cs
@@ -0,0 +1,18 @@
+﻿using Game.World;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.GameControl
+{
+    public class MainSceneWorld : MonoBehaviour
+    {
+        public void Initialize(GameController game_controller)
+        {
+            foreach(var world_object in this.transform.GetComponentsInChildren<IWorldObject>())
+            {
+                world_object.Initialize(game_controller);
+            }
+        }
+    }
+}
