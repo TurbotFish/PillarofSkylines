4dc6ebbf7b65b359db749435a0713490332bda14
Namerian <email@email.shit>
2018-06-17 15:36:42 +0200

    tutorial exit is now persistent

---
+++ Assets/Scripts/Model/PersistentData/TutorialExitPersistentData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Model
+{
+    public class TutorialExitPersistentData : PersistentData
+    {
+        public bool IsOpen { get; set; }
+
+        public TutorialExitPersistentData(string unique_id) : base(unique_id)
+        {
+
+        }
+    }
+} // end of namespace
