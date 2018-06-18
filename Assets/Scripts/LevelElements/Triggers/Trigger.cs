e1497decbf2b3e50039fa083f535616c023a1f2a
grreuze <grreuze@gmail.com>
2018-06-18 11:19:21 +0200

    bah alors

--- Assets/Scripts/LevelElements/Triggers/Trigger.cs
+++ Assets/Scripts/LevelElements/Triggers/Trigger.cs
@@ -75,7 +75,7 @@ namespace Game.LevelElements
 
             isInitialized = true;
 
-            SetTriggerState(_triggerState, true);
+            //SetTriggerState(_triggerState, true); // ??
         }
 
         /// <summary>
