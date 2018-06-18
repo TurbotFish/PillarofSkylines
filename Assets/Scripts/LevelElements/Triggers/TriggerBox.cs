9b36fabc5a0fc91db452e0557724ba1082fc1851
Namerian <email@email.shit>
2018-06-18 13:26:06 +0200

    working on triggers

--- Assets/Scripts/LevelElements/Triggers/TriggerBox.cs
+++ Assets/Scripts/LevelElements/Triggers/TriggerBox.cs
@@ -5,47 +5,129 @@ using Game.GameControl;
 namespace Game.LevelElements
 {
     [RequireComponent(typeof(BoxCollider))]
-    public class TriggerBox : Trigger
+    public class TriggerBox : Trigger, IInteractable
     {
         //###########################################################
 
-        [SerializeField]
-        string tagToActivate = "Player";
-        [SerializeField]
-        bool definitiveActivation;
-        [SerializeField]
-        float delayBeforeDeactivation;
-        [SerializeField]
-        bool directionIndependent;
+        // -- CONSTANTS
 
-        [SerializeField]
-        bool changeMaterial;
+        [Header("TriggerBox")]
+        [SerializeField] private string tagToActivate = "Player";
+        [SerializeField] private bool definitiveActivation;
+        [SerializeField] private float delayBeforeDeactivation;
+        [SerializeField] private bool directionIndependent;
+        [SerializeField] private bool changeMaterial;
 
-        [ConditionalHide("changeMaterial"), SerializeField]
-        int materialID = 0;
-
-        [ConditionalHide("changeMaterial"), SerializeField]
-        Material on, off;
-
-        [ConditionalHide("changeMaterial"), SerializeField]
-        new Renderer renderer;
+        [ConditionalHide("changeMaterial"), SerializeField] private int materialID = 0;
+        [ConditionalHide("changeMaterial"), SerializeField] private Material on;
+        [ConditionalHide("changeMaterial"), SerializeField] private Material off;
+        [ConditionalHide("changeMaterial"), SerializeField] private new Renderer renderer;
 
 
-		[Header("Sound")]
-		public bool playsSoundOnStart;
+        [Header("Sound")]
+        public bool playsSoundOnStart;
         public AudioClip onClip;
         public AudioClip offClip;
         public float minDistanceStart = 10f;
-		public float maxDistanceStart = 50f;
-		[Range(0,2)] public float volume = 1f;
-		public float clipDuration = 0f;
-		public bool addRandomisation = false;
+        public float maxDistanceStart = 50f;
+        [Range(0, 2)] public float volume = 1f;
+        public float clipDuration = 0f;
+        public bool addRandomisation = false;
+
         //###########################################################
 
-        #region editor methods
+        // -- ATTRIBUTES
 
-#if UNITY_EDITOR
+        private Transform MyTransform;
 
+        //###########################################################
+
+        // -- INITIALIZATION
+
+        public override void Initialize(GameController gameController)
+        {
+            base.Initialize(gameController);
+
+            GetComponent<BoxCollider>().isTrigger = true;
+        }
+
+        //###########################################################
+
+        // -- INQUIRIES
+
+        public Transform Transform
+        {
+            get
+            {
+                if (MyTransform == null)
+                {
+                    MyTransform = this.transform;
+                }
+
+                return MyTransform;
+            }
+        }
+
+        public bool IsInteractable()
+        {
+            return false;
+        }
+
+        //###########################################################
+
+        // -- OPERATIONS
+
+        public void OnPlayerEnter()
+        {
+            /*
+             * gravity check
+             */
+            if (Mathf.Abs(Vector3.Dot(GameController.PlayerController.Transform.up, transform.up)) > 0.9f || directionIndependent)
+            {
+                ActivateTriggerBox();
+            }
+        }
+
+        public void OnPlayerExit()
+        {
+            StartCoroutine(DeactivateTriggerBoxCoroutine());
+        }
+
+        public void OnHoverBegin()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnHoverEnd()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnInteraction()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Called when the state of the trigger changes.
+        /// </summary>
+        /// <param name="old_state"></param>
+        /// <param name="new_state"></param>
+        protected override void OnTriggerStateChanged(bool old_state, bool new_state)
+        {
+            if (TriggerState)
+            {
+                ActivateTriggerBox();
+            }
+            else
+            {
+                StartCoroutine(DeactivateTriggerBoxCoroutine());
+            }
+        }
+
+        /// <summary>
+        /// EDITOR: OnDrawGizmos
+        /// </summary>
         protected override void OnDrawGizmos()
         {
             base.OnDrawGizmos();
@@ -60,107 +142,202 @@ namespace Game.LevelElements
             Gizmos.DrawWireCube(box.center, box.size);
         }
 
-#endif
-
-        #endregion editor methods
-
-        //###########################################################
-
-        #region monobehaviour methods
-
-        private void OnTriggerEnter(Collider other)
+        /// <summary>
+        /// Activates the TriggerBox.
+        /// </summary>
+        private void ActivateTriggerBox()
         {
-            if (other.tag == tagToActivate && (Mathf.Abs(Vector3.Dot(other.transform.up, transform.up)) > 0.9f || directionIndependent))
+            if (definitiveActivation && playsSoundOnStart && !TriggerState)
             {
+                SoundifierOfTheWorld.PlaySoundAtLocation(onClip, transform, maxDistanceStart, volume, minDistanceStart, clipDuration, addRandomisation);
+            }
 
-                if (definitiveActivation && playsSoundOnStart && !TriggerState)
+            if (Toggle)
+            {
+                SetTriggerState(!TriggerState);
+            }
+            else
+            {
+                SetTriggerState(true);
+            }
+
+            if (changeMaterial)
+            {
+                Material[] sharedMaterialsCopy = renderer.sharedMaterials;
+                sharedMaterialsCopy[materialID] = TriggerState ? on : off;
+                renderer.sharedMaterials = sharedMaterialsCopy;
+            }
+
+            if (playsSoundOnStart && !definitiveActivation)
+            {
+                if (TriggerState)
                 {
                     SoundifierOfTheWorld.PlaySoundAtLocation(onClip, transform, maxDistanceStart, volume, minDistanceStart, clipDuration, addRandomisation);
                 }
-
-                if (Toggle)
-                    SetTriggerState(!TriggerState);
                 else
-                    SetTriggerState(true);
-
-
-                if (changeMaterial)
                 {
-                    Material[] sharedMaterialsCopy = renderer.sharedMaterials;
-                    sharedMaterialsCopy[materialID] = TriggerState ? on : off;
-                    renderer.sharedMaterials = sharedMaterialsCopy;
+                    SoundifierOfTheWorld.PlaySoundAtLocation(offClip, transform, maxDistanceStart, volume, minDistanceStart, clipDuration, addRandomisation);
                 }
+            }
 
-                if (playsSoundOnStart && !definitiveActivation)
+            GlyphFX _fx = GetComponent<GlyphFX>();
+            if (_fx != null)
+            {
+                if (Toggle)
                 {
                     if (TriggerState)
-    					SoundifierOfTheWorld.PlaySoundAtLocation (onClip, transform, maxDistanceStart, volume, minDistanceStart, clipDuration, addRandomisation);
+                    {
+                        _fx.GlyphOn();
+                    }
                     else
-                        SoundifierOfTheWorld.PlaySoundAtLocation (offClip, transform, maxDistanceStart, volume, minDistanceStart, clipDuration, addRandomisation);
-
+                    {
+                        _fx.GlyphOff();
+                    }
                 }
-
-                GlyphFX _fx = GetComponent<GlyphFX> ();
-				if (_fx != null) {
-					if (Toggle) {
-						
-						if (TriggerState) {
-							_fx.GlyphOn ();
-						} else {
-							_fx.GlyphOff ();
-						}
-
-
-					} else {
-						_fx.GlyphOn ();
-					}
-
-
-				} else {
-					//Debug.LogError ("whoopsie, " + transform.name + " isn't attached to a glyph, is it ?");
-				}
+                else
+                {
+                    _fx.GlyphOn();
+                }
             }
-        }
-
-        private IEnumerator OnTriggerExit(Collider other)
-        {
-            if (definitiveActivation || Toggle) yield break;
-            if (other.tag == tagToActivate)
+            else
             {
-                yield return new WaitForSeconds(delayBeforeDeactivation);
-                SetTriggerState(false);
-
-				if (changeMaterial) {
-					Material[] sharedMaterialsCopy = renderer.sharedMaterials;
-					sharedMaterialsCopy [materialID] = off;
-					renderer.sharedMaterials = sharedMaterialsCopy;
-				}
-
-
-				GlyphFX _fx = GetComponent<GlyphFX> ();
-				if (_fx != null) {
-					_fx.GlyphOff ();
-				} else {
-					Debug.LogWarning ("whoopsie, " + transform.name + " isn't attached to a glyph, is it ?");
-				}
+                //Debug.LogError ("whoopsie, " + transform.name + " isn't attached to a glyph, is it ?");
             }
         }
 
-        #endregion monobehaviour methods
-
-        //###########################################################
-
-        #region public methods
-
-        public override void Initialize(GameController gameController)
+        /// <summary>
+        /// Deactivates the TriggerBox.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator DeactivateTriggerBoxCoroutine()
         {
-            base.Initialize(gameController);
+            if (definitiveActivation || Toggle)
+            {
+                yield break;
+            }
 
-            GetComponent<BoxCollider>().isTrigger = true;
+            yield return new WaitForSeconds(delayBeforeDeactivation);
+            SetTriggerState(false);
+
+            if (changeMaterial)
+            {
+                Material[] sharedMaterialsCopy = renderer.sharedMaterials;
+                sharedMaterialsCopy[materialID] = off;
+                renderer.sharedMaterials = sharedMaterialsCopy;
+            }
+
+            GlyphFX _fx = GetComponent<GlyphFX>();
+            if (_fx != null)
+            {
+                _fx.GlyphOff();
+            }
+            else
+            {
+                Debug.LogWarning("whoopsie, " + transform.name + " isn't attached to a glyph, is it ?");
+            }
         }
 
-        #endregion public methods
+        ///// <summary>
+        ///// Monobehaviour: OnTriggerEnter
+        ///// </summary>
+        ///// <param name="other"></param>
+        //private void OnTriggerEnter(Collider other)
+        //{
+        //    if (other.tag == tagToActivate && (Mathf.Abs(Vector3.Dot(other.transform.up, transform.up)) > 0.9f || directionIndependent))
+        //    {
 
-        //###########################################################
+        //        if (definitiveActivation && playsSoundOnStart && !TriggerState)
+        //        {
+        //            SoundifierOfTheWorld.PlaySoundAtLocation(onClip, transform, maxDistanceStart, volume, minDistanceStart, clipDuration, addRandomisation);
+        //        }
+
+        //        if (Toggle)
+        //            SetTriggerState(!TriggerState);
+        //        else
+        //            SetTriggerState(true);
+
+
+        //        if (changeMaterial)
+        //        {
+        //            Material[] sharedMaterialsCopy = renderer.sharedMaterials;
+        //            sharedMaterialsCopy[materialID] = TriggerState ? on : off;
+        //            renderer.sharedMaterials = sharedMaterialsCopy;
+        //        }
+
+        //        if (playsSoundOnStart && !definitiveActivation)
+        //        {
+        //            if (TriggerState)
+        //                SoundifierOfTheWorld.PlaySoundAtLocation(onClip, transform, maxDistanceStart, volume, minDistanceStart, clipDuration, addRandomisation);
+        //            else
+        //                SoundifierOfTheWorld.PlaySoundAtLocation(offClip, transform, maxDistanceStart, volume, minDistanceStart, clipDuration, addRandomisation);
+
+        //        }
+
+        //        GlyphFX _fx = GetComponent<GlyphFX>();
+        //        if (_fx != null)
+        //        {
+        //            if (Toggle)
+        //            {
+
+        //                if (TriggerState)
+        //                {
+        //                    _fx.GlyphOn();
+        //                }
+        //                else
+        //                {
+        //                    _fx.GlyphOff();
+        //                }
+
+
+        //            }
+        //            else
+        //            {
+        //                _fx.GlyphOn();
+        //            }
+
+
+        //        }
+        //        else
+        //        {
+        //            //Debug.LogError ("whoopsie, " + transform.name + " isn't attached to a glyph, is it ?");
+        //        }
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Monobehaviour: OnTriggerExit
+        ///// </summary>
+        ///// <param name="other"></param>
+        ///// <returns></returns>
+        //private IEnumerator OnTriggerExit(Collider other)
+        //{
+        //    if (definitiveActivation || Toggle)
+        //    {
+        //        yield break;
+        //    }
+        //    else if (other.tag == tagToActivate)
+        //    {
+        //        yield return new WaitForSeconds(delayBeforeDeactivation);
+        //        SetTriggerState(false);
+
+        //        if (changeMaterial)
+        //        {
+        //            Material[] sharedMaterialsCopy = renderer.sharedMaterials;
+        //            sharedMaterialsCopy[materialID] = off;
+        //            renderer.sharedMaterials = sharedMaterialsCopy;
+        //        }
+
+
+        //        GlyphFX _fx = GetComponent<GlyphFX>();
+        //        if (_fx != null)
+        //        {
+        //            _fx.GlyphOff();
+        //        }
+        //        else
+        //        {
+        //            Debug.LogWarning("whoopsie, " + transform.name + " isn't attached to a glyph, is it ?");
+        //        }
+        //    }
+        //}
     }
 } //end of namespace
